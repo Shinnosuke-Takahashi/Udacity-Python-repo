6373d0342220ce09cdfb6f879f6199da851b657c
Shinnosuke Takahashi <shinnosuketakahashisemail@gmail.com>
2018-10-15 20:14:17 -0400

    updated header

--- searchEngine.py
+++ searchEngine.py
@@ -1,7 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> #THIS CODE IS CURRENTLY UNDER DEVELOPMENT AND IS UPDATED REGULARLY.
+#THIS CODE IS CURRENTLY UNDER DEVELOPMENT AND IS UPDATED REGULARLY.
 # This is a search engine under development.
 # THE FOLLOWING IS A LOG OF UPDATES:
 # 10-9-18: defined crawl_web()
